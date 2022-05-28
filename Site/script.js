0882a7982617d3f7e7522357cdefc6e269e88957
RuanSousa25 <103215747+RuanSousa25@users.noreply.github.com>
2022-05-27 22:45:52 -0300

    ajustei coisas e melhorei design

--- Site/script.js
+++ Site/script.js
@@ -1,7 +1,17 @@
-$.getJSON("http://dreamlo.com/lb/6289978d8f40bb11c05666db/json", (data)=>{
-    for(let i in data.dreamlo.leaderboard.entry){
-      let el = document.getElementById("leaderboardcont");
-      el.innerHTML += "<div class=posrank><h4>"+ data.dreamlo.leaderboard.entry[i].name+"-"+data.dreamlo.leaderboard.entry[i].score+""+"</h4></div>";
-    }
-    console.log(data.dreamlo.leaderboard.entry)
- });
+$.getJSON("http://dreamlo.com/lb/6289978d8f40bb11c05666db/json", (data) => {
+  for (let i in data.dreamlo.leaderboard.entry) {
+    let el = document.getElementById("leaderboardcont");
+    el.innerHTML +=
+      "<div class=posrank id='posrank" +
+      (Number(i) + 1) +
+      "'><h3>" +
+      (Number(i) + 1) +
+      "</h3><h4>" +
+      data.dreamlo.leaderboard.entry[i].name +
+      "-" +
+      data.dreamlo.leaderboard.entry[i].score +
+      "" +
+      "</h4></div>";
+  }
+  console.log(data.dreamlo.leaderboard.entry);
+});
